efaecb6407168945c1fb0add065a2fc9d910460f
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-07-29 19:45:59 -0400

    actualizacion

--- excuses.js
+++ excuses.js
@@ -1,11 +1,11 @@
 
 function callExcuse() {
 
-  let who = ['El perro','Mi Abuela','Su tortuga','Mi pajaro'];
-  let what = ['se lo comio','meo','choco','se rompio'];
+  let who = ['El perro','Mi Abuela','Su tortuga','Mi pajaro',"El dragon"];
+  let what = ['se lo comio','meo','choco','se rompio','lo quemo'];
   let when = ['antes de clases.','justo en el momento.','cuando termine.','durante mi almuezo.','mientras resaba.'];
 
-  let index= Math.floor(Math.random()*4);
+  let index= Math.floor(Math.random()*5);
 
   let primera=[who[index],what[index],when[index]];
 
